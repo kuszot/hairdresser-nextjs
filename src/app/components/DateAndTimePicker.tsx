444bca0cdba12875ce6aa4f25acc85453a6f0a06
kuszot <kuszot123@gmail.com>
2025-01-25 00:29:53 +0100

    add Date and Time Picker wrapper component

---
+++ src/app/components/DateAndTimePicker.tsx
@@ -0,0 +1,23 @@
+'use client';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+import 'dayjs/locale/pl';
+import dynamic from 'next/dynamic';
+
+const Calendar = dynamic(() => import('./Calendar'), { ssr: false });
+const TimePicker = dynamic(() => import('./TimePicker'), { ssr: false });
+
+const DateAndTimePicker = () => {
+    const today:dayjs.Dayjs = dayjs(new Date()) ;
+  return (
+    <div>
+        <LocalizationProvider adapterLocale="pl" dateAdapter={AdapterDayjs}>
+            <Calendar currentDate={today}/>
+            <TimePicker currentDate={today}/>
+        </LocalizationProvider>
+    </div>
+  )
+}
+
+export default DateAndTimePicker
