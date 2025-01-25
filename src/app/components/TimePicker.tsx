635e68770892819c4d74976153f9f4caa77b812e
kuszot <kuszot123@gmail.com>
2025-01-25 01:50:42 +0100

    add syle to Time Picker component

--- src/app/components/TimePicker.tsx
+++ src/app/components/TimePicker.tsx
@@ -1,17 +1,65 @@
 'use client';
 import dayjs from 'dayjs';
 import { MobileTimePicker } from '@mui/x-date-pickers/MobileTimePicker';
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { PickersActionBar } from '@mui/x-date-pickers/PickersActionBar';
 interface TimePickerProps {
-  currentDate: dayjs.Dayjs
+  currentDate: dayjs.Dayjs;
+  
+}
+
+declare module '@mui/material/styles' {
+  interface Components {
+    [key: string]: any
+  }
 }
 
 const TimePicker = ({currentDate} : TimePickerProps) => {
+  const theme = createTheme({
+    components: {
+      MuiInputBase: {
+        styleOverrides: {
+          root: {
+            color: 'white',
+          },
+        },
+      },
+      MuiOutlinedInput: {
+        styleOverrides: {
+          notchedOutline : {
+            borderColor: 'rgba(255, 255, 255, 0.23)',
+          },
+          root: {
+            '&:hover .MuiOutlinedInput-notchedOutline': {
+              borderColor: 'white',
+            },
+          },
+        },
+      },
+      MuiFormLabel: {
+        styleOverrides: {
+          root: {
+            color: 'white',
+          },
+        },
+      }, 
+    }
+  });
   return (
-    <div>
-        <MobileTimePicker
-            defaultValue={currentDate}
-        />
+    <div className='flex justify-center'>
+        <ThemeProvider theme={theme}>
+          <MobileTimePicker
+              defaultValue={currentDate}
+              label="Godzina"
+              localeText={{
+                cancelButtonLabel: 'Zamknij',
+                okButtonLabel: 'Zapisz',
+                toolbarTitle: 'Wybierz datę'
+
+              }}
+              
+          />
+        </ThemeProvider>
     </div>
   )
 }
