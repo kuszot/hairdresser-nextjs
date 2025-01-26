f4f9800be05d5bacdd0645ba226caff5e76763a0
kuszot <kuszot123@gmail.com>
2025-01-26 01:21:40 +0100

    change calendar component view and logic

--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -1,71 +1,93 @@
 'use client';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
-import 'dayjs/locale/pl';
-import dayjs from 'dayjs';
+import { ScheduleXCalendar } from '@schedule-x/react'
+import {
+    createCalendar,
+    createViewDay,
+    createViewMonthAgenda,
+    createViewMonthGrid,
+    createViewWeek,
+} from '@schedule-x/calendar'
+import { createEventsServicePlugin } from '@schedule-x/events-service'
+
+import '@schedule-x/theme-default/dist/index.css'
+import {useEffect, useState } from 'react'
+
+// pluggins
+import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop'
+import { createCurrentTimePlugin } from '@schedule-x/current-time'
+import { createResizePlugin } from '@schedule-x/resize'
+import { createEventModalPlugin } from '@schedule-x/event-modal'
+
+import AddEventModal from './AddEventModal';
+
+function CalendarApp() {
+  const currentDate : Date = new Date();
+
+  // initialize plugins
+  const eventsService = useState(() => createEventsServicePlugin())[0]
+  const dragAndDropPlugin = createDragAndDropPlugin();
+  const currentTimePlugin = createCurrentTimePlugin();
+  const resizePlugin = createResizePlugin();
+  const eventModalPlugin = createEventModalPlugin();
+
+  const [events, setEvents] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [newEvent, setNewEvent] = useState<object>({});
+
+//   fetch events
+  useEffect(() => {
+    const fetchEvents = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/events');
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            setEvents(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    fetchEvents();
+  }, [])
 
 
-declare module '@mui/material/styles' {
-  interface Components {
-    [key: string]: any
+  const handleDateClick = (dateTime : string) => {
+    setIsModalOpen(true);
+    setNewEvent({
+        title: '',
+        start: dateTime,
+    });
   }
-}
-
-interface CalendarProps {
-  currentDate: dayjs.Dayjs
-}
-
-const Calendar = ({currentDate}: CalendarProps) => {
-  const theme = createTheme({
-    components: {
-      MuiDateCalendar: {
-        styleOverrides: {
-          root: {
-            color: 'white',
-
-          },
+  
+ 
+  const calendar = createCalendar({
+    views: [createViewDay(), createViewWeek(), createViewMonthGrid(), createViewMonthAgenda()],
+    events: events,
+    isDark: true,
+    isResponsive: true,
+    plugins: [eventsService,dragAndDropPlugin,currentTimePlugin,resizePlugin,eventModalPlugin],
+    locale: 'pl-PL',
+    selectedDate: currentDate.toISOString().split('T')[0],
+    callbacks: {
+        onClickDateTime(dateTime) {
+            handleDateClick(dateTime);
         },
-      },
-      MuiPickersDay: {
-        styleOverrides: {
-          root: {
-            color: 'white',
-            backgroundColor: 'transparent',
-          },
-          today: {
-            border: '1px white solid !important'
-          },
-        },
-      },
-      
-      MuiTypography: {
-        styleOverrides: {
-          root: {
-            color: 'white !important',
-          },
-        },
-      },
-      MuiSvgIcon: {
-        styleOverrides: {
-          root: {
-            color: 'white',
-          },
-        },
-      },
-    },
-  });
+    }
+  })
+ 
+  useEffect(() => {
+    // get all events
+    eventsService.getAll()
+  }, [])
+ 
   return (
-      <ThemeProvider theme={theme}>
-        <DateCalendar
-           sx={{
-            '& .MuiPickersDay-root.Mui-selected': {
-              border: '0 !important',
-            },
-          }}
-          defaultValue={currentDate}
-        />
-      </ThemeProvider>
+    <div>
+      {isModalOpen && <AddEventModal/>}
+      <ScheduleXCalendar calendarApp={calendar} />
+    </div>
   )
 }
-
-export default Calendar
+ 
+export default CalendarApp
